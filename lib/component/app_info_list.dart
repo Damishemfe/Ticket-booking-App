76df164c85a6f3cfc6d02e2fd0413fc02426d0c9
Dami Shemfe <victarnen@gmail.com>
2022-07-28 15:27:49 +0530

    Added a search screen, getx dependency and also mapped data

---
+++ lib/component/app_info_list.dart
@@ -0,0 +1,53 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'one.png',
+    'place': 'Steffans',
+    'destination': 'Jos, Nigeria',
+    'price': '10,000'
+  },
+  {
+    'image': 'three.png',
+    'place': 'Eko Hotel',
+    'destination': 'Lagos, Nigeria',
+    'price': '50,000'
+  },
+  {
+    'image': 'two.png',
+    'place': 'Sheraton',
+    'destination': 'Abuja, Nigeria',
+    'price': '100,000'
+  },
+  {
+    'image': 'one.png',
+    'place': 'Open Space',
+    'destination': 'London, UK',
+    'price': '500,000'
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {'code': "JOS", 'name': "Plateau"},
+    'to': {'code': "LAG", 'name': "Lagos"},
+    'flying_time': '3H 30M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    'number': 23
+  },
+  {
+    'from': {'code': "ABJ", 'name': "Abuja"},
+    'to': {'code': "LAG", 'name': "Lagos"},
+    'flying_time': '1H 30M',
+    'date': "10 MAY",
+    'departure_time': "08:30 PM",
+    'number': 40
+  },
+  {
+    'from': {'code': "JOS", 'name': "Plateau"},
+    'to': {'code': "PORT", 'name': "Port Harcourt"},
+    'flying_time': '5H 30M',
+    'date': "1 JULY",
+    'departure_time': "11:00 AM",
+    'number': 15
+  }
+];
